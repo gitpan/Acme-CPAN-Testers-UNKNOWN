42e67aa14554db726833f72ac6b0f1e1820ac857
Chris Williams <chris@bingosnet.co.uk>
2011-07-13 13:05:05 +0000

    Import of BINGOS/Acme-CPAN-Testers-UNKNOWN-0.02 from CPAN.
    gitpan-cpan-distribution: Acme-CPAN-Testers-UNKNOWN
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         BINGOS/Acme-CPAN-Testers-UNKNOWN-0.02.tar.gz
    gitpan-cpan-author:       BINGOS
    gitpan-cpan-maturity:     released

---
+++ lib/Acme/CPAN/Testers/UNKNOWN.pm
@@ -0,0 +1,23 @@
+package Acme::CPAN::Testers::UNKNOWN;
+
+use strict;
+use warnings;
+use vars qw[$VERSION];
+
+$VERSION = '0.02';
+
+q[semper propositum];
+
+=pod
+
+=head1 NAME
+
+Acme::CPAN::Testers::UNKNOWN - generate an UNKNOWN test report
+
+=head1 DESCRIPTION
+
+Acme::CPAN::Testers::UNKNOWN generates an C<UNKNOWN> test report when run by a CPAN Tester.
+
+That's pretty much it, really.
+
+=cut
